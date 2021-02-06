9e6b87ae2d20e43b6b1ba6b2a02bd1f4d22c0454
Maks <pineapple6934@gmail.com>
2021-02-06 12:33:11 +0500

    2

--- LuckyNum/MainWindow.xaml.cs
+++ LuckyNum/MainWindow.xaml.cs
@@ -36,32 +36,4 @@ namespace LuckyNum
             else LblOut.Content = "Неверное число";
         }
     }
-    public class Num
-    {
-        public int[] nums = new int[6];
-        public bool isParsed { get; set; }
-        public Num(string input)
-        {
-            char[] chars = input.ToArray();
-            try
-            {
-                for (int i = 0; i < 6; i++)
-                {
-                    nums[i] = int.Parse(chars[i].ToString());
-                }
-                isParsed = true;
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Неверные данные");
-                isParsed = false;
-            }
-        }
-
-        public bool isLucky()
-        {
-            if (nums[0] + nums[1] + nums[2] == nums[3] + nums[4] + nums[5]) return true;
-            else return false;
-        }
-    }
 }
